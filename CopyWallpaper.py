84cb16bbe13748bdfac2b8c885eb9d83fac93b5b
海猿 <30338214+CCGGAAG@users.noreply.github.com>
2023-05-15 21:14:27 +0800

    Update CopyWallpaper.py

--- CopyWallpaper.py
+++ CopyWallpaper.py
@@ -163,14 +163,14 @@ class CopyWallpaper:
             yaml.dump(data, fw)
             fw.close()
 
-
+            
 if __name__ == '__main__':
-    test = CopyWallpaper("D:/Program Files (x86)/Steam/steamapps/workshop/content/431960", "D:/test")
+    # 配置并初始化
+    wallpaper_dir = "D:/Program Files (x86)/Steam/steamapps/workshop/content/431960"
+    output_dir = "D:/test"
+    test = CopyWallpaper(wallpaper_dir, output_dir)
     # 全部类型拷贝
     test.start_get_wallpaper_file(pkg_only=True, mp4_only=True, new_file_only=True)
-
-    # new_file_only项是仅拷贝新订阅的壁纸文件，需要先把本地已经订阅的壁纸统计一下：init_installed_file_list
-    # 然后再运行获取壁纸文件命令时，就会跳过这些已经记录的壁纸文件
 
     # 仅图片类型拷贝
     # test.start_get_wallpaper_file(pkg_only=True)
@@ -178,5 +178,10 @@ if __name__ == '__main__':
     # 仅视频类型拷贝
     # test.start_get_wallpaper_file(mp4_only=True)
 
-    # 初始化不拷贝文件列表（把已经订阅的壁纸加入隔离名单，以后不会再下载）
+    # 统计不拷贝文件列表（把已经订阅的壁纸加入隔离名单，以后不会再下载）
     # test.init_installed_file_list()
+    """
+    new_file_only项是仅拷贝新订阅的壁纸文件，需要先把本地已经订阅的壁纸统计一下：init_installed_file_list
+    然后再运行获取壁纸文件命令时，就会跳过这些已经记录的壁纸文件
+
+    """
